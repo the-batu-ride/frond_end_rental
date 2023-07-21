89b467c8e241c59b7d28a6b5440520d4230494c6
Rendisaputra33 <gorengmbah@gmail.com>
2023-07-21 19:43:14 +0700

    add-Qr-Screen

---
+++ lib/screens/QrScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QrScreen extends StatefulWidget {
+  const QrScreen({super.key});
+
+  @override
+  State<QrScreen> createState() => _QrScreenState();
+}
+
+class _QrScreenState extends State<QrScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/widget/newsItems.dart
+++ lib/widget/newsItems.dart
@@ -6,13 +6,6 @@ Widget renderNewsItem({
 }) {
   return GestureDetector(
     onTap: () {},
-    child: Card(
-      width: 200,
-      heightImage: 140,
-      imageProvider: AssetImage('assets/mockup.png'),
-      tags: [_tag('Category', () {}), _tag('Product', () {})],
-      title: _title(),
-      description: _content(),
-    ),
+    child: Card(),
   );
 }
