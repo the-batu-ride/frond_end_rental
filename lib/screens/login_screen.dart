eedd11aa61a26b53e5f2397cc436a7c8be014c9f
syasul <githubku4@gmail.com>
2023-07-25 23:37:58 +0700

    add

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:frond_end_rental/constant/colors.dart';
-import 'package:frond_end_rental/constant/conection.dart';
-import 'package:frond_end_rental/models/login_model.dart';
 
 import '../constant/conection.dart';
 import '../models/login_model.dart';
@@ -104,7 +101,7 @@ class Login extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),
